75051f16ce9397c3187b5be39347bfda0e3d5419
subhash983 <subhash.983@gmail.com>
2018-03-23 01:40:54 +0530

    server authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,14 +1,46 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timeStamp = new Date().getTime();
+    return jwt.encode({
+        sub: user.id,
+        iat: timeStamp
+    }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(422).send({error: 'You must provide email id and password'});
+    }
+
     User.findOne({
         email: email
     }, function(err, existingUser) {
-        console.log(existingUser);
+        if (err) {
+            return next(err);
+        }
+
+        if (existingUser) {
+            return res.status(422).send({error: 'Email is in use'});
+        }
+
+        const user = new User({email: email, password: password});
+        user.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.json({token: tokenForUser(user)});
+        });
     });
-    console.log(email);
-    res.send({success: true});
-}
+};
+
+exports.signin = function(req, res, next) {
+    res.send({
+        token: tokenForUser(req.user)
+    });
+};
